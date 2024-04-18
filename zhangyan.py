9ade54d85a678bab77c569f78898edfa8a6230a3
ZHANG YAN <111035712+zhangyan14@users.noreply.github.com>
2024-04-18 23:25:41 +0900

    Add files via upload

--- zhangyan.py
+++ zhangyan.py
@@ -2,24 +2,25 @@ from ultralytics import YOLO
 import cv2
 # Load a model
 
-model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
-video_path = "D:/workspace/mp4/test.mp4"
+# model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
-cap = cv2.VideoCapture(video_path)
+# video_path = "D:/workspace/mp4/test.mp4"
 
-while cap.isOpened():
-    status, frame = cap.read()
-    if not status:
-        break
-    results = model.predict(source = frame)
-    result = results[0]
-    anno_frame = result.plot()
-    cv2.imshow("V8",anno_frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows
+# cap = cv2.VideoCapture(video_path)
+
+# while cap.isOpened():
+#     status, frame = cap.read()
+#     if not status:
+#         break
+#     results = model.predict(source = frame)
+#     result = results[0]
+#     anno_frame = result.plot()
+#     cv2.imshow("V8",anno_frame)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+# cap.release()
+# cv2.destroyAllWindows
 
 # Use the model
 
@@ -28,3 +29,19 @@ cv2.destroyAllWindows
 # save_path = "D:\workspace\mp4"
 # results = model.predict(source, stream=True)
 # print(results)
+
+model = YOLO("yolov8n.pt")
+source = 'D:/workspace/mp4/641.webp'
+# results = model("https://ultralytics.com/images/bus.jpg")
+# results = model(source) 
+
+# for result in results:
+#     boxes = result.boxes  # Boxes object for bounding box outputs
+#     masks = result.masks  # Masks object for segmentation masks outputs
+#     keypoints = result.keypoints  # Keypoints object for pose outputs
+#     probs = result.probs  # Probs object for classification outputs
+#     # result.show()  # display to screen
+#     # result.save(filename='result.jpg')  # save to disk
+
+model.predict(source, save=True)
+
